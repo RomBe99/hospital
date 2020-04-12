38682ece96e3eb4794612944e14495a276e1ef91
Roman <romanbelinsky99@gmail.com>
2020-04-12 18:25:20 +0600

    Update class Patient: 1) Add fields email, address, phone

--- src/main/java/net/thumbtack/hospital/model/Patient.java
+++ src/main/java/net/thumbtack/hospital/model/Patient.java
@@ -6,53 +6,66 @@ import java.util.Objects;
 
 // TODO Добавить валидацию через аннотации
 public class Patient extends User {
+    // FIXME Добавить недостающие поля
+    private String email;
+    private String address;
+    private String phone;
     private List<ScheduleCell> tickets;
 
     public Patient(int id,
                    String login, String password,
                    String firstName, String lastName, String patronymic,
-                   List<ScheduleCell> tickets) {
+                   String email, String address, String phone, List<ScheduleCell> tickets) {
         super(id, login, password, firstName, lastName, patronymic);
-        this.tickets = tickets;
+
+        setEmail(email);
+        setAddress(address);
+        setPhone(phone);
+        setTickets(tickets);
     }
 
     public Patient(int id,
                    String login, String password,
-                   String firstName, String lastName, String patronymic) {
-        this(id, login, password, firstName, lastName, patronymic, new ArrayList<>());
+                   String firstName, String lastName, String patronymic,
+                   String email, String address, String phone) {
+        this(id, login, password, firstName, lastName, patronymic, email, address, phone, new ArrayList<>());
     }
 
     public Patient(String login, String password,
-                   String firstName, String lastName, String patronymic) {
-        this(0, login, password, firstName, lastName, patronymic);
+                   String firstName, String lastName, String patronymic,
+                   String email, String address, String phone) {
+        this(0, login, password, firstName, lastName, patronymic, email, address, phone);
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public void setTickets(List<ScheduleCell> tickets) {
         this.tickets = tickets;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
     public List<ScheduleCell> getTickets() {
         return tickets;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Patient)) return false;
-        if (!super.equals(o)) return false;
-        Patient patient = (Patient) o;
-        return Objects.equals(getTickets(), patient.getTickets());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getTickets());
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + "\nPatient{" +
-                "tickets=" + tickets +
-                '}';
     }
 }
