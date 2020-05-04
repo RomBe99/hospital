700bc6fcf92c6958fd446908d7ab6c8119085513
Roman <romanbelinsky99@gmail.com>
2020-05-05 02:40:59 +0600

    Remove @Id, @WeekDay work with Integer

--- src/main/java/net/thumbtack/hospital/util/validator/WeekDayConstraintValidator.java
+++ src/main/java/net/thumbtack/hospital/util/validator/WeekDayConstraintValidator.java
@@ -6,11 +6,11 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.DayOfWeek;
 
-public class WeekDayConstraintValidator implements ConstraintValidator<WeekDay, String> {
+public class WeekDayConstraintValidator implements ConstraintValidator<WeekDay, Integer> {
    @Override
-   public boolean isValid(String weekDay, ConstraintValidatorContext context) {
+   public boolean isValid(Integer weekDay, ConstraintValidatorContext context) {
       try {
-         DayOfWeek.valueOf(weekDay);
+         DayOfWeek.of(weekDay);
       } catch (IllegalArgumentException | NullPointerException ex) {
          return false;
       }
