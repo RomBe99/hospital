afdb8aa81c8ed1ad127c2021e8fd29be2fe045c6
Roman <romanbelinsky99@gmail.com>
2020-05-24 20:43:20 +0600

    Add tests for UserDaoImpl

--- src/test/java/net/thumbtack/hospital/BaseTest.java
+++ src/test/java/net/thumbtack/hospital/BaseTest.java
@@ -3,24 +3,31 @@ package net.thumbtack.hospital;
 import net.thumbtack.hospital.dao.AdminDao;
 import net.thumbtack.hospital.dao.DoctorDao;
 import net.thumbtack.hospital.dao.PatientDao;
+import net.thumbtack.hospital.dao.UserDao;
 import net.thumbtack.hospital.daoimpl.AdminDaoImpl;
 import net.thumbtack.hospital.daoimpl.DoctorDaoImpl;
 import net.thumbtack.hospital.daoimpl.PatientDaoImpl;
+import net.thumbtack.hospital.daoimpl.UserDaoImpl;
 import net.thumbtack.hospital.debug.DebugDao;
 import net.thumbtack.hospital.debug.DebugDaoImpl;
 import net.thumbtack.hospital.model.Administrator;
 import net.thumbtack.hospital.model.Doctor;
 import net.thumbtack.hospital.model.Patient;
+import net.thumbtack.hospital.util.error.PermissionDeniedException;
 import net.thumbtack.hospital.util.mybatis.MyBatisUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.UUID;
 
 public class BaseTest {
     protected AdminDao adminDao = new AdminDaoImpl();
     protected DoctorDao doctorDao = new DoctorDaoImpl();
     protected PatientDao patientDao = new PatientDaoImpl();
     private final DebugDao debugDao = new DebugDaoImpl();
+    private final UserDao userDao = new UserDaoImpl();
 
     private static boolean setUpIsDone = false;
 
@@ -49,6 +56,39 @@ public class BaseTest {
         debugDao.clearTimeCells();
         debugDao.clearUsers();
         debugDao.clearMedicalCommissions();
+    }
+
+    @Test(expected = PermissionDeniedException.class)
+    public void loginAndLogoutTest() throws PermissionDeniedException {
+        String login = "VorobevaAltina312";
+        String password = "fwZO9tIyAQlf";
+        String firstName = "Альтина";
+        String lastName = "Воробьёва";
+        String patronymic = "Максимовна";
+        String position = "Test Admin";
+
+        Administrator expectedAdmin = new Administrator(login, password, firstName, lastName, patronymic, position);
+
+        try {
+            Administrator insertedAdmin = insertAdministrator(login, password, firstName, lastName, patronymic, position);
+            expectedAdmin.setId(insertedAdmin.getId());
+
+            Assert.assertEquals(expectedAdmin, insertedAdmin);
+
+            String sessionId = UUID.randomUUID().toString();
+            int adminIdWhenLogin = userDao.login(sessionId, expectedAdmin.getLogin(), expectedAdmin.getPassword());
+
+            Assert.assertEquals(insertedAdmin.getId(), adminIdWhenLogin);
+
+            int idAfterCheckingPermissions = userDao.hasPermissions(sessionId);
+
+            Assert.assertEquals(adminIdWhenLogin, idAfterCheckingPermissions);
+
+            userDao.logout(sessionId);
+            userDao.hasPermissions(sessionId);
+        } catch (RuntimeException e) {
+            Assert.fail();
+        }
     }
 
     protected Administrator insertAdministrator(String login, String password,
