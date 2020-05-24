360b85fc9002100fe93b0ea536732b2cb34725d6
Roman <romanbelinsky99@gmail.com>
2020-05-25 05:21:50 +0600

    Add enum WeekDays

--- src/main/java/net/thumbtack/hospital/util/validator/WeekDayConstraintValidator.java
+++ src/main/java/net/thumbtack/hospital/util/validator/WeekDayConstraintValidator.java
@@ -1,16 +1,17 @@
 package net.thumbtack.hospital.util.validator;
 
+import net.thumbtack.hospital.util.WeekDays;
 import net.thumbtack.hospital.util.validator.annotation.WeekDay;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.DayOfWeek;
 
-public class WeekDayConstraintValidator implements ConstraintValidator<WeekDay, Integer> {
+public class WeekDayConstraintValidator implements ConstraintValidator<WeekDay, String> {
    @Override
-   public boolean isValid(Integer weekDay, ConstraintValidatorContext context) {
+   public boolean isValid(String weekDay, ConstraintValidatorContext context) {
       try {
-         DayOfWeek.of(weekDay);
+         WeekDays.valueOf(weekDay);
       } catch (IllegalArgumentException | NullPointerException ex) {
          return false;
       }
