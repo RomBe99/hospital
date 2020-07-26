65198bf9cb1276c2123f759bcaf9dc4f94187bbc
Roman <romanbelinsky99@gmail.com>
2020-07-26 15:45:11 +0600

    1. Add ErrorsDtoResponse with error list. Now all methods exception handlers use it. 2. Rework GlobalControllerExceptionHandler.handleFieldValidationExceptions. Now user get all validation errors.

--- src/main/java/net/thumbtack/hospital/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/net/thumbtack/hospital/controller/GlobalControllerExceptionHandler.java
@@ -1,17 +1,21 @@
 package net.thumbtack.hospital.controller;
 
 import net.thumbtack.hospital.dtoresponse.other.ErrorDtoResponse;
+import net.thumbtack.hospital.dtoresponse.other.ErrorsDtoResponse;
 import net.thumbtack.hospital.util.error.ErrorMessageFactory;
 import net.thumbtack.hospital.util.error.PermissionDeniedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Objects;
+import java.util.*;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
@@ -25,22 +29,33 @@ public class GlobalControllerExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
-    public ErrorDtoResponse handleFieldValidationExceptions(MethodArgumentNotValidException ex) {
-        String errorCode = ex.getBindingResult().getAllErrors().stream().findFirst().get().getDefaultMessage();
-        String field = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getField();
-        String errorMessage = errorMessageFactory.getValidationMessageByCode(errorCode);
+    public ErrorsDtoResponse handleFieldValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        List<ErrorDtoResponse> result = new ArrayList<>(errors.size());
 
-        return new ErrorDtoResponse(errorCode, field, errorMessage);
+        String errorCode;
+        String field;
+        String errorMessage;
+
+        for (ObjectError e : errors) {
+            errorCode = e.getDefaultMessage();
+            field = ((FieldError) e).getField();
+            errorMessage = errorMessageFactory.getValidationMessageByCode(errorCode);
+
+            result.add(new ErrorDtoResponse(errorCode, field, errorMessage));
+        }
+
+        return new ErrorsDtoResponse(result);
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(PermissionDeniedException.class)
     @ResponseBody
-    public ErrorDtoResponse handlePermissionExceptions(PermissionDeniedException ex) {
+    public ErrorsDtoResponse handlePermissionExceptions(PermissionDeniedException ex) {
         String errorCode = ex.getErrorCode().getErrorCode();
         String field = "sessionId";
         String errorMessage = "You do not have the required permissions.";
 
-        return new ErrorDtoResponse(errorCode, field, errorMessage);
+        return new ErrorsDtoResponse(Collections.singletonList(new ErrorDtoResponse(errorCode, field, errorMessage)));
     }
 }
