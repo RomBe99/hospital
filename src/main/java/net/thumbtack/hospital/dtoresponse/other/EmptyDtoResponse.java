29628031eca3b17da590704e0bd6ca39b1453ef2
Belinsky Roman <romanbelinsky99@gmail.com>
2021-02-22 02:18:08 +0600

    Add method EmptyDtoResponse.equals

--- src/main/java/net/thumbtack/hospital/dtoresponse/other/EmptyDtoResponse.java
+++ src/main/java/net/thumbtack/hospital/dtoresponse/other/EmptyDtoResponse.java
@@ -1,4 +1,9 @@
 package net.thumbtack.hospital.dtoresponse.other;
 
 public class EmptyDtoResponse {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        return o != null && getClass() == o.getClass();
+    }
 }
