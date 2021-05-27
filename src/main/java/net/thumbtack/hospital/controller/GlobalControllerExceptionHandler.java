d9eb64990ffdc807932067f61979cb348c636dd6
Belinsky Roman <romanbelinsky99@gmail.com>
2021-05-28 02:19:17 +0600

    Refactor GlobalControllerExceptionHandler

--- src/main/java/net/thumbtack/hospital/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/net/thumbtack/hospital/controller/GlobalControllerExceptionHandler.java
@@ -6,18 +6,23 @@ import net.thumbtack.hospital.util.cookie.CookieFactory;
 import net.thumbtack.hospital.util.error.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
     private final ErrorMessageFactory errorMessageFactory;
+    private final Map<ScheduleErrorCode, String> scheduleErrorCodeToFields = Map.of(
+            ScheduleErrorCode.SCHEDULE_HAVE_APPOINTMENT, "schedule",
+            ScheduleErrorCode.ALREADY_CONTAINS_APPOINTMENT, "date and time"
+    );
 
     @Autowired
     public GlobalControllerExceptionHandler(ErrorMessageFactory errorMessageFactory) {
@@ -28,34 +33,23 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     public ErrorsDtoResponse handleFieldValidationExceptions(MethodArgumentNotValidException ex) {
-        final var errors = ex.getBindingResult().getFieldErrors();
-        final var result = new ArrayList<ErrorDtoResponse>(errors.size());
-
-        String errorCode;
-        String field;
-        String errorMessage;
-
-        for (var e : errors) {
-            errorCode = e.getDefaultMessage();
-            field = e.getField();
-            errorMessage = errorMessageFactory.getValidationMessageByCode(errorCode);
-
-            result.add(new ErrorDtoResponse(errorCode, field, errorMessage));
-        }
-
-        return new ErrorsDtoResponse(result);
+        return new ErrorsDtoResponse(
+                ex.getBindingResult().getFieldErrors().stream()
+                        .map(fieldError -> new ErrorDtoResponse(
+                                fieldError.getDefaultMessage(),
+                                fieldError.getField(),
+                                errorMessageFactory.getValidationMessageByCode(fieldError.getDefaultMessage())
+                        ))
+                        .collect(Collectors.toList())
+        );
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ScheduleException.class)
     @ResponseBody
     public ErrorsDtoResponse handleScheduleExceptions(ScheduleException ex) {
-        final var errorCodeToField = new HashMap<ScheduleErrorCode, String>();
-        errorCodeToField.put(ScheduleErrorCode.SCHEDULE_HAVE_APPOINTMENT, "schedule");
-        errorCodeToField.put(ScheduleErrorCode.ALREADY_CONTAINS_APPOINTMENT, "date and time");
-
         final var errorCode = ex.getErrorCode().getErrorCode();
-        final var field = errorCodeToField.get(ex.getErrorCode());
+        final var field = scheduleErrorCodeToFields.get(ex.getErrorCode());
         final var errorMessage = ex.getErrorCode().getErrorMessage();
 
         return new ErrorsDtoResponse(Collections.singletonList(new ErrorDtoResponse(errorCode, field, errorMessage)));
